fa4df3b595d659e895f77fbab58e4aae433e13de
tinnq <tinnq22@gmail.com>
2023-09-20 17:26:56 +0200

    feat: use temp files

--- services/audio-combinar.ts
+++ services/audio-combinar.ts
@@ -1,9 +1,20 @@
 import { UploadedFile } from "express-fileupload";
+import temp from 'temp';
 const audioconcat = require("audioconcat");
 
 export class AudioCombinar {
   combine(files: UploadedFile[]) {
-    audioconcat(files.map((file: UploadedFile) => file.name))
+    temp.track();
+    const paths: string[] = [];
+
+    for (const file of files) {
+      const stream = temp.createWriteStream();
+      stream.write(file.data);
+      paths.push(stream.path as string)
+      stream.end();
+    }
+
+    audioconcat(paths)
       .concat("./outputs/combined.mp3")
       .on("error", function (err: Error) {
         console.error("Error:", err);
